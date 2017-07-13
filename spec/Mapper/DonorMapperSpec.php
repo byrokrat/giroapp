68d3d79ee021e0800b58316f2b4cf033c3444576
panina <panina@nonbinary.me>
2017-07-13 22:27:19 +0200

    and updated spec to schema names

--- spec/Mapper/DonorMapperSpec.php
+++ spec/Mapper/DonorMapperSpec.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 namespace spec\byrokrat\giroapp\Mapper;
 
 use byrokrat\giroapp\Mapper\DonorMapper;
-use byrokrat\giroapp\Mapper\Arrayizer\DonorArrayizer;
+use byrokrat\giroapp\Mapper\Schema\DonorSchema;
 use byrokrat\giroapp\Model\Donor;
 use hanneskod\yaysondb\CollectionInterface;
 use hanneskod\yaysondb\FilterableInterface;
@@ -15,9 +15,9 @@ use Prophecy\Argument;
 
 class DonorMapperSpec extends ObjectBehavior
 {
-    function let(CollectionInterface $collection, DonorArrayizer $donorArrayizer)
+    function let(CollectionInterface $collection, DonorSchema $donorSchema)
     {
-        $this->beConstructedWith($collection, $donorArrayizer);
+        $this->beConstructedWith($collection, $donorSchema);
     }
 
     function it_is_initializable()
@@ -25,12 +25,12 @@ class DonorMapperSpec extends ObjectBehavior
         $this->shouldHaveType(DonorMapper::CLASS);
     }
 
-    function it_finds_donors_by_key($collection, $donorArrayizer, Donor $donor)
+    function it_finds_donors_by_key($collection, $donorSchema, Donor $donor)
     {
         $collection->has('foobar')->willReturn(true);
         $collection->read('foobar')->willReturn(['foobar']);
 
-        $donorArrayizer->fromArray(['foobar'])->willReturn($donor);
+        $donorSchema->fromArray(['foobar'])->willReturn($donor);
         
         $this->findByKey('foobar')->shouldEqual($donor);
     }
@@ -42,28 +42,28 @@ class DonorMapperSpec extends ObjectBehavior
         $this->shouldThrow(\RuntimeException::CLASS)->during('findByKey',['foobar']);
     }
 
-    function it_can_save_donor($collection, $donorArrayizer, Donor $donor)
+    function it_can_save_donor($collection, $donorSchema, Donor $donor)
     {
         $donor->getMandateKey()->willReturn('foobar');
 
-        $donorArrayizer->toArray($donor)->willReturn(['asdf']);
+        $donorSchema->toArray($donor)->willReturn(['asdf']);
 
         $collection->insert(['asdf'], 'foobar')->shouldBeCalled();
 
         $this->save($donor);
     }
 
-    function it_can_find_all($collection, $donorArrayizer, Donor $donor1, Donor $donor2)
+    function it_can_find_all($collection, $donorSchema, Donor $donor1, Donor $donor2)
     {
         $collection->getIterator()->willReturn(new \ArrayIterator([['foo'], ['bar']]));
 
-        $donorArrayizer->fromArray(['foo'])->willReturn($donor1);
-        $donorArrayizer->fromArray(['bar'])->willReturn($donor2);
+        $donorSchema->fromArray(['foo'])->willReturn($donor1);
+        $donorSchema->fromArray(['bar'])->willReturn($donor2);
 
         $this->findAll()->shouldReturn([$donor1, $donor2]);
     }
 
-    function it_can_find_active_donor($collection, $donorArrayizer, Donor $donor)
+    function it_can_find_active_donor($collection, $donorSchema, Donor $donor)
     {
         $collection->first(
             Argument::type(
@@ -71,12 +71,12 @@ class DonorMapperSpec extends ObjectBehavior
             )
         )->willReturn(['foobar']);
 
-        $donorArrayizer->fromArray(['foobar'])->willReturn($donor);
+        $donorSchema->fromArray(['foobar'])->willReturn($donor);
 
         $this->findByActivePayerNumber('foobar')->shouldEqual($donor);
     }
 
-    function it_can_find_by_payernumber($collection, $donorArrayizer, Donor $donor1, Donor $donor2, FilterableInterface $filterableInterface)
+    function it_can_find_by_payernumber($collection, $donorSchema, Donor $donor1, Donor $donor2, FilterableInterface $filterableInterface)
     {
         $collection->find(
             Argument::type(
@@ -88,8 +88,8 @@ class DonorMapperSpec extends ObjectBehavior
             new \ArrayIterator([['foo'], ['bar']])
         );
 
-        $donorArrayizer->fromArray(['foo'])->willReturn($donor1);
-        $donorArrayizer->fromArray(['bar'])->willReturn($donor2);
+        $donorSchema->fromArray(['foo'])->willReturn($donor1);
+        $donorSchema->fromArray(['bar'])->willReturn($donor2);
 
         $this->findByPayerNumber('foobar')->shouldReturn([$donor1, $donor2]);
     }
