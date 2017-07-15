ab2b27c50789c59e1aa6776b0759700bc57ae7d0
Hannes Forsgård <hannes.forsgard@fripost.org>
2017-07-15 11:12:37 +0200

    Finalize MandateResponseListener

--- spec/Listener/MandateResponseListenerSpec.php
+++ spec/Listener/MandateResponseListenerSpec.php
@@ -6,19 +6,57 @@ namespace spec\byrokrat\giroapp\Listener;
 
 use byrokrat\giroapp\Listener\MandateResponseListener;
 use byrokrat\giroapp\Mapper\DonorMapper;
-use byrokrat\giroapp\Event\NodeEvent;
 use byrokrat\giroapp\Model\Donor;
+use byrokrat\giroapp\Events;
+use byrokrat\giroapp\Event\NodeEvent;
+use byrokrat\giroapp\Event\LogEvent;
+use byrokrat\giroapp\Event\DonorEvent;
+use byrokrat\giroapp\Model\DonorState\MandateApprovedState;
+use byrokrat\giroapp\Model\DonorState\InactiveState;
+use byrokrat\giroapp\Model\DonorState\ErrorState;
 use byrokrat\id\Id;
 use byrokrat\banking\AccountNumber;
 use byrokrat\autogiro\Tree\Node;
+use byrokrat\autogiro\Messages;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class MandateResponseListenerSpec extends ObjectBehavior
 {
-    function let(DonorMapper $donorMapper)
-    {
+    function let(
+        NodeEvent $event,
+        Node $parentNode,
+        Node $payerNumberNode,
+        Node $idNode,
+        Node $accountNode,
+        Node $infoNode,
+        Node $commentNode,
+        Donor $donor,
+        DonorMapper $donorMapper
+    ) {
+        $event->getNode()->willReturn($parentNode);
+
+        $parentNode->getChild('payer_number')->willReturn($payerNumberNode);
+        $payerNumberNode->getValue()->willReturn('payer-number');
+
+        $parentNode->getChild('id')->willReturn($idNode);
+        $idNode->hasAttribute('id')->willReturn(false);
+
+        $parentNode->getChild('account')->willReturn($accountNode);
+        $accountNode->hasAttribute('account')->willReturn(false);
+
+        $parentNode->getChild('info')->willReturn($infoNode);
+        $infoNode->getValue()->willReturn('');
+
+        $parentNode->getChild('comment')->willReturn($commentNode);
+        $commentNode->getValue()->willReturn('');
+
+        $donorMapper->findByActivePayerNumber('payer-number')->willReturn($donor);
+
+        $donor->getPayerNumber()->willReturn('');
+        $donor->getMandateKey()->willReturn('');
+
         $this->beConstructedWith($donorMapper);
     }
 
@@ -27,49 +65,190 @@ class MandateResponseListenerSpec extends ObjectBehavior
         $this->shouldHaveType(MandateResponseListener::CLASS);
     }
 
-    function it_fails_if_mandate_key_and_payer_number_does_not_match(
-        NodeEvent $event,
-        Id $id,
-        AccountNumber $account,
-        Node $node,
-        Donor $donor,
-        EventDispatcherInterface $dispatcher,
-        $donorMapper
+    function it_fails_if_node_contains_invalid_id(
+        $event,
+        $idNode,
+        $donor,
+        Id $nodeId,
+        Id $donorId,
+        EventDispatcherInterface $dispatcher
     ) {
-        $this->setup_mandate_response('node-value', $event, $id, $account, $node, $donor, $donorMapper);
+        $idNode->hasAttribute('id')->willReturn(true);
+        $idNode->getAttribute('id')->willReturn($nodeId);
 
-        $donor->getPayerNumber()->willReturn('not-the-same-as-payer-number-node-value');
+        $donor->getDonorId()->willReturn($donorId);
 
-        $this->shouldThrow(\RuntimeException::CLASS)->during('__invoke', [$event, '', $dispatcher]);
+        $nodeId->format('S-sk')->willReturn('foo');
+        $donorId->format('S-sk')->willReturn('NOT-foo');
+
+        $dispatcher->dispatch(Events::WARNING_EVENT, Argument::type(LogEvent::CLASS))->shouldBeCalled();
+        $event->stopPropagation()->shouldBeCalled();
+
+        $this->__invoke($event, '', $dispatcher);
     }
 
-    private function setup_mandate_response(
-        string $nodeValue,
-        NodeEvent $event,
-        Id $id,
-        AccountNumber $account,
-        Node $node,
-        Donor $donor,
-        $donorMapper,
-        string $madateKey = 'some-mandate-key'
+    function it_fails_if_node_contains_invalid_account(
+        $event,
+        $accountNode,
+        $donor,
+        AccountNumber $nodeAccount,
+        AccountNumber $donorAccount,
+        EventDispatcherInterface $dispatcher
     ) {
-        $event->getNode()->willReturn($node);
+        $accountNode->hasAttribute('account')->willReturn(true);
+        $accountNode->getAttribute('account')->willReturn($nodeAccount);
 
-        $node->getChild('id')->willReturn($node);
-        $node->getChild('account')->willReturn($node);
-        $node->getChild('payer_number')->willReturn($node);
-        $node->getChild('info')->willReturn($node);
-        $node->getChild('comment')->willReturn($node);
+        $donor->getAccount()->willReturn($donorAccount);
 
-        $node->getAttribute('id')->willReturn($id);
-        $node->getAttribute('account')->willReturn($account);
+        $donorAccount->equals($nodeAccount)->willReturn(false);
 
-        $node->getValue()->willReturn($nodeValue);
+        $nodeAccount->getNumber()->willReturn('');
+        $donorAccount->getNumber()->willReturn('');
 
-        $keyBuilder->buildKey($id, $account)->willReturn($madateKey);
+        $dispatcher->dispatch(Events::WARNING_EVENT, Argument::type(LogEvent::CLASS))->shouldBeCalled();
+        $event->stopPropagation()->shouldBeCalled();
 
-        $donorMapper->findByKey($madateKey)->willReturn($donor);
+        $this->__invoke($event, '', $dispatcher);
+    }
 
-        $donor->getPayerNumber()->willReturn($nodeValue);
+    function it_fails_on_unknown_response_code(
+        $event,
+        $infoNode,
+        EventDispatcherInterface $dispatcher
+    ) {
+        $infoNode->getValue()->willReturn('this-is-not-a-valid-autogiro-reposnose-code');
+
+        $dispatcher->dispatch(Events::WARNING_EVENT, Argument::type(LogEvent::CLASS))->shouldBeCalled();
+        $event->stopPropagation()->shouldBeCalled();
+
+        $this->__invoke($event, '', $dispatcher);
+    }
+
+    function it_sets_error_state_on_updated_payer_number(
+        $event,
+        $infoNode,
+        $donor,
+        $donorMapper,
+        EventDispatcherInterface $dispatcher
+    ) {
+        $infoNode->getValue()->willReturn(Messages::MANDATE_UPDATED_PAYER_NUMBER_BY_RECIPIENT);
+
+        $donor->setState(Argument::type(ErrorState::CLASS))->shouldBeCalled();
+        $dispatcher->dispatch(Events::MANDATE_INVALID_EVENT, Argument::type(DonorEvent::CLASS))->shouldBeCalled();
+        $donorMapper->save($donor)->shouldBeCalled();
+
+        $this->__invoke($event, '', $dispatcher);
+    }
+
+    function it_sets_error_state_on_specific_mandate_response_from_bank(
+        $event,
+        $infoNode,
+        $donor,
+        $donorMapper,
+        EventDispatcherInterface $dispatcher
+    ) {
+        $infoNode->getValue()->willReturn(Messages::MANDATE_ACCOUNT_RESPONSE_FROM_BANK);
+
+        $donor->setState(Argument::type(ErrorState::CLASS))->shouldBeCalled();
+        $dispatcher->dispatch(Events::MANDATE_INVALID_EVENT, Argument::type(DonorEvent::CLASS))->shouldBeCalled();
+        $donorMapper->save($donor)->shouldBeCalled();
+
+        $this->__invoke($event, '', $dispatcher);
+    }
+
+    function it_sets_error_state_on_mandate_deleted_due_to_unanswered_request(
+        $event,
+        $infoNode,
+        $donor,
+        $donorMapper,
+        EventDispatcherInterface $dispatcher
+    ) {
+        $infoNode->getValue()->willReturn(Messages::MANDATE_DELETED_DUE_TO_UNANSWERED_ACCOUNT_REQUEST);
+
+        $donor->setState(Argument::type(ErrorState::CLASS))->shouldBeCalled();
+        $dispatcher->dispatch(Events::MANDATE_INVALID_EVENT, Argument::type(DonorEvent::CLASS))->shouldBeCalled();
+        $donorMapper->save($donor)->shouldBeCalled();
+
+        $this->__invoke($event, '', $dispatcher);
+    }
+
+    function it_sets_inactive_state_on_mandate_deleted_by_payer(
+        $event,
+        $infoNode,
+        $donor,
+        $donorMapper,
+        EventDispatcherInterface $dispatcher
+    ) {
+        $infoNode->getValue()->willReturn(Messages::MANDATE_DELETED_BY_PAYER);
+
+        $donor->setState(Argument::type(InactiveState::CLASS))->shouldBeCalled();
+        $dispatcher->dispatch(Events::MANDATE_REVOKED_EVENT, Argument::type(DonorEvent::CLASS))->shouldBeCalled();
+        $donorMapper->save($donor)->shouldBeCalled();
+
+        $this->__invoke($event, '', $dispatcher);
+    }
+
+    function it_sets_inactive_state_on_mandate_deleted_by_recipient(
+        $event,
+        $infoNode,
+        $donor,
+        $donorMapper,
+        EventDispatcherInterface $dispatcher
+    ) {
+        $infoNode->getValue()->willReturn(Messages::MANDATE_DELETED_BY_RECIPIENT);
+
+        $donor->setState(Argument::type(InactiveState::CLASS))->shouldBeCalled();
+        $dispatcher->dispatch(Events::MANDATE_REVOKED_EVENT, Argument::type(DonorEvent::CLASS))->shouldBeCalled();
+        $donorMapper->save($donor)->shouldBeCalled();
+
+        $this->__invoke($event, '', $dispatcher);
+    }
+
+    function it_sets_inactive_state_on_mandate_deleted_due_to_closed_recipient_bg(
+        $event,
+        $infoNode,
+        $donor,
+        $donorMapper,
+        EventDispatcherInterface $dispatcher
+    ) {
+        $infoNode->getValue()->willReturn(Messages::MANDATE_DELETED_DUE_TO_CLOSED_RECIPIENT_BG);
+
+        $donor->setState(Argument::type(InactiveState::CLASS))->shouldBeCalled();
+        $dispatcher->dispatch(Events::MANDATE_REVOKED_EVENT, Argument::type(DonorEvent::CLASS))->shouldBeCalled();
+        $donorMapper->save($donor)->shouldBeCalled();
+
+        $this->__invoke($event, '', $dispatcher);
+    }
+
+    function it_sets_inactive_state_on_mandate_deleted_due_to_closed_payer_bg(
+        $event,
+        $infoNode,
+        $donor,
+        $donorMapper,
+        EventDispatcherInterface $dispatcher
+    ) {
+        $infoNode->getValue()->willReturn(Messages::MANDATE_DELETED_DUE_TO_CLOSED_PAYER_BG);
+
+        $donor->setState(Argument::type(InactiveState::CLASS))->shouldBeCalled();
+        $dispatcher->dispatch(Events::MANDATE_REVOKED_EVENT, Argument::type(DonorEvent::CLASS))->shouldBeCalled();
+        $donorMapper->save($donor)->shouldBeCalled();
+
+        $this->__invoke($event, '', $dispatcher);
+    }
+
+    function it_sets_approved_state_on_mandate_created(
+        $event,
+        $infoNode,
+        $donor,
+        $donorMapper,
+        EventDispatcherInterface $dispatcher
+    ) {
+        $infoNode->getValue()->willReturn(Messages::MANDATE_CREATED_BY_RECIPIENT);
+
+        $donor->setState(Argument::type(MandateApprovedState::CLASS))->shouldBeCalled();
+        $dispatcher->dispatch(Events::MANDATE_APPROVED_EVENT, Argument::type(DonorEvent::CLASS))->shouldBeCalled();
+        $donorMapper->save($donor)->shouldBeCalled();
+
+        $this->__invoke($event, '', $dispatcher);
     }
 }
--- src/Listener/MandateResponseListener.php
+++ src/Listener/MandateResponseListener.php
@@ -23,12 +23,13 @@ declare(strict_types = 1);
 namespace byrokrat\giroapp\Listener;
 
 use byrokrat\giroapp\Mapper\DonorMapper;
+use byrokrat\giroapp\Events;
 use byrokrat\giroapp\Event\DonorEvent;
 use byrokrat\giroapp\Event\LogEvent;
 use byrokrat\giroapp\Event\NodeEvent;
-use byrokrat\giroapp\Model\DonorState\MandateSentState;
 use byrokrat\giroapp\Model\DonorState\MandateApprovedState;
 use byrokrat\giroapp\Model\DonorState\InactiveState;
+use byrokrat\giroapp\Model\DonorState\ErrorState;
 use byrokrat\autogiro\Messages;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
@@ -51,49 +52,57 @@ class MandateResponseListener
     {
         $node = $nodeEvent->getNode();
 
-        // TODO är har jag börjat använda findByActivePayerNumber..
-        // men det kräver på något sätt att det bara kan finnas ett aktivt payer number...
-        // det måste göras någon form av kontroll av detta när nya medgivanden skrivs till databasen...
-        //
-        // $donorMapper->has($donor); // kan behövs??
-        //
-        // Eller framförallt handlar det om att göra någon avancerad defensiv kontroll...
-        //
-        // fundera.. Issue i github??
-        //
-        // Vad betyder active? Att status är annat än inactive??
-
         $donor = $this->donorMapper->findByActivePayerNumber(
             $node->getChild('payer_number')->getValue()
         );
 
-        // TODO om jag kastar massa undantag varje gång något är fel
-        // så kommer jag aldrig att kunna processa andra noder som innehåller korrekta saker...
-        // alltså förbud mot att kasta undantag
+        // validate id if present in node
+        if ($node->getChild('id')->hasAttribute('id')) {
+            /** @var \byrokrat\id\Id */
+            $nodeId = $node->getChild('id')->getAttribute('id');
 
+            if ($donor->getDonorId()->format('S-sk') != $nodeId->format('S-sk')) {
+                $dispatcher->dispatch(
+                    Events::WARNING_EVENT,
+                    new LogEvent(
+                        sprintf(
+                            "Invalid mandate response for payer number '%s', found donor id '%s', expecting '%s'",
+                            $donor->getPayerNumber(),
+                            $nodeId->format('S-sk'),
+                            $donor->getDonorId()->format('S-sk')
+                        )
+                    )
+                );
 
-        // TODO det här är fel, om mandate exempelvis är deleted av payer så kommer
-        // state att vara något annat.. Alltså ska detta vara en egen valideringsfunkion..
-        //
-        // $this->inState(MandateSentState::CLASS, $donor);
-        /*
-            // Vill jag skriva det såhär???
-            // Ja det är en bra ide. Implementera på en gång. Det kan behövas även på andra ställen...
-            if (!$donor->inState(MandateSentState::CLASS)) {
+                // stop processing on invalid id
+                $nodeEvent->stopPropagation();
+                return;
             }
-        */
-        // fundera över vid vilka situationer nedad som assertState() kan vara aktuellt...
-        if (!$donor->getState() instanceof MandateSentState::CLASS) {
-            $dispatcher->dispatch(
-                Events::ERROR_EVENT,
-                new LogEvent("Unexpected mandate response ... TODO")
-            );
-            return;
         }
-        //
-        //
-        //
 
+        // validate account if present in node
+        if ($node->getChild('account')->hasAttribute('account')) {
+            /** @var \byrokrat\banking\AccountNumber */
+            $nodeAccount = $node->getChild('account')->getAttribute('account');
+
+            if (!$donor->getAccount()->equals($nodeAccount)) {
+                $dispatcher->dispatch(
+                    Events::WARNING_EVENT,
+                    new LogEvent(
+                        sprintf(
+                            "Invalid mandate response for payer number '%s', found account '%s', expecting '%s'",
+                            $donor->getPayerNumber(),
+                            $nodeAccount->getNumber(),
+                            $donor->getAccount()->getNumber()
+                        )
+                    )
+                );
+
+                // stop processing on invalid account
+                $nodeEvent->stopPropagation();
+                return;
+            }
+        }
 
         $infoCode = $node->getChild('info')->getValue();
 
@@ -108,8 +117,6 @@ class MandateResponseListener
 
         switch ($infoCode) {
             case Messages::MANDATE_DELETED_BY_PAYER:
-                $this->validateDonorIdAndAccount($donor, $node);
-                // intentional fall-through to revoke mandate
             case Messages::MANDATE_DELETED_BY_RECIPIENT:
             case Messages::MANDATE_DELETED_DUE_TO_CLOSED_RECIPIENT_BG:
             case Messages::MANDATE_DELETED_DUE_TO_CLOSED_PAYER_BG:
@@ -118,55 +125,26 @@ class MandateResponseListener
                 break;
 
             case Messages::MANDATE_CREATED_BY_RECIPIENT:
-                $this->validateDonorIdAndAccount($donor, $node);
                 $donor->setState(new MandateApprovedState);
                 $dispatcher->dispatch(Events::MANDATE_APPROVED_EVENT, $donorEvent);
                 break;
 
             case Messages::MANDATE_UPDATED_PAYER_NUMBER_BY_RECIPIENT:
-                $this->validateDonorIdAndAccount($donor, $node);
-                // TODO add mandateKey eller vad som helst till meddelande...
-                $dispatcher->dispatch(
-                    Events::ERROR_EVENT,
-                    new LogEvent('Found an update payer number post, not supported by giroapp.')
-                );
-                return;
-
             case Messages::MANDATE_ACCOUNT_RESPONSE_FROM_BANK:
             case Messages::MANDATE_DELETED_DUE_TO_UNANSWERED_ACCOUNT_REQUEST:
-                $this->validateDonorIdAndAccount($donor, $node);
-                // TODO ErrorState hör detta. Något som gått utanför ramen och behöver fixxas...
-                // japp det är en riktigt bra idé!
-                $donor->setState(new InactiveState);
+                $donor->setState(new ErrorState);
                 $dispatcher->dispatch(Events::MANDATE_INVALID_EVENT, $donorEvent);
                 break;
 
             default:
                 $dispatcher->dispatch(
-                    Events::ERROR_EVENT,
+                    Events::WARNING_EVENT,
                     new LogEvent("Invalid mandate response info code: $infoCode")
                 );
+                $nodeEvent->stopPropagation();
                 return;
         }
 
         $this->donorMapper->save($donor);
-    }
-
-    private function validateDonorIdAndAccount(Donor $donor, Node $node)
-    {
-        // TODO kan inte detta helt enkelt göras ifall attribute finns...
-        // hur ser trädet ut ifall node inte innehåller id/account???
-        // jag tror att det blir mer hållbart i längden..
-
-        // TODO finns det något som motsvarar equals för id??
-        if ($donor->getDonorId() != $node->getChild('id')->getAttribute('id')) {
-            // TODO ett trivsamt felmeddelande!
-            throw new \RuntimeException();
-        }
-
-        if (!$donor->getAccount()->equals($node->getChild('account')->getAttribute('account'))) {
-            // TODO ett trivsamt felmeddelande!
-            throw new \RuntimeException();
-        }
     }
 }
