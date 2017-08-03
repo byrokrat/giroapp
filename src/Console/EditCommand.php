2dc5a91d89e27773946a82ea74487428a1eed142
sverker wahlin <panina@nonbinary.me>
2017-08-03 22:39:56 +0200

    added missing include

--- src/Console/EditCommand.php
+++ src/Console/EditCommand.php
@@ -26,6 +26,7 @@ use byrokrat\giroapp\Mapper\DonorMapper;
 use byrokrat\giroapp\Builder\DonorBuilder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
