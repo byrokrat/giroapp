bd41b84ce5deda3faa7e56175c002efce59df67d
Hannes Forsgård <hannes.forsgard@fripost.org>
2017-08-10 11:32:25 +0200

    Create OutputtingListener

---
+++ spec/Listener/OutputtingListenerSpec.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace spec\byrokrat\giroapp\Listener;
+
+use byrokrat\giroapp\Listener\OutputtingListener;
+use byrokrat\giroapp\Event\LogEvent;
+use Symfony\Component\Console\Output\OutputInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class OutputtingListenerSpec extends ObjectBehavior
+{
+    function let(OutputInterface $output)
+    {
+        $this->beConstructedWith($output);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(OutputtingListener::CLASS);
+    }
+
+    function it_writes_warning_messages(LogEvent $event, $output)
+    {
+        $event->getMessage()->willReturn('foobar');
+        $this->onWarningEvent($event);
+        $output->writeln('foobar')->shouldHaveBeenCalled();
+    }
+
+    function it_writes_info_messages(LogEvent $event, $output)
+    {
+        $event->getMessage()->willReturn('foobar');
+        $this->onInfoEvent($event);
+        $output->writeln('foobar')->shouldHaveBeenCalled();
+    }
+
+    function it_writes_debug_message_if_output_is_verbose(LogEvent $event, $output)
+    {
+        $event->getMessage()->willReturn('foobar');
+
+        $output->isVerbose()->willReturn(true);
+        $output->writeln('foobar')->shouldBeCalled();
+
+        $this->onDebugEvent($event);
+    }
+
+    function it_discards_debug_message_if_output_is_not_verbose(LogEvent $event, $output)
+    {
+        $event->getMessage()->willReturn('foobar');
+
+        $output->isVerbose()->willReturn(false);
+        $output->writeln('foobar')->shouldNotBeCalled();
+
+        $this->onDebugEvent($event);
+    }
+}
---
+++ src/Listener/OutputtingListener.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of byrokrat\giroapp.
+ *
+ * byrokrat\giroapp is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * byrokrat\giroapp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with byrokrat\giroapp. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2016-17 Hannes Forsgård
+ */
+
+declare(strict_types = 1);
+
+namespace byrokrat\giroapp\Listener;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use byrokrat\giroapp\Event\LogEvent;
+
+/**
+ * Write warning, info and debug messages to output
+ */
+class OutputtingListener
+{
+    /**
+     * @var OutputInterface
+     */
+    private $output;
+
+    public function __construct(OutputInterface $output)
+    {
+        $this->output = $output;
+    }
+
+    public function onWarningEvent(LogEvent $event)
+    {
+        $this->output->writeln($event->getMessage());
+    }
+
+    public function onInfoEvent(LogEvent $event)
+    {
+        $this->output->writeln($event->getMessage());
+    }
+
+    public function onDebugEvent(LogEvent $event)
+    {
+        if ($this->output->isVerbose()) {
+            $this->output->writeln($event->getMessage());
+        }
+    }
+}
